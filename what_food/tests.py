8fe68156d3243f53e86c93a443bb2a18538b150b
sujine2 <52918738+sujine2@users.noreply.github.com>
2020-09-13 15:24:58 +0900

    Update tests.py

--- what_food/tests.py
+++ what_food/tests.py
@@ -1,3 +1,28 @@
-from django.test import TestCase
+from django.test import TestCase, Client
 
-# Create your tests here.
+
+class FoodTestCase(TestCase):
+    def test_view_index(self):
+        c = Client()
+        resp = c.get("/")
+        self.assertTrue(resp.status_code,200)
+
+    def test_view_w(self):
+        c = Client()
+        resp = c.get("/w")
+        self.assertTrue(resp.status_code,200)
+
+    def test_view_j(self):
+        c = Client()
+        resp = c.get("/j")
+        self.assertTrue(resp.status_code,200)
+
+    def test_view_c(self):
+        c = Client()
+        resp = c.get("/c")
+        self.assertTrue(resp.status_code,200)
+
+    def test_view_k(self):
+        c = Client()
+        resp = c.get("/k")
+        self.assertTrue(resp.status_code,200)
